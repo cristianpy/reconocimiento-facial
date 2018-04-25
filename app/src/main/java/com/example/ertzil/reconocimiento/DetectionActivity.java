d35ccdf25c5cd0ef440327e4c44000c60db0e2cf
cristianpy <cristianramonpaniagua@gmail.com>
2018-04-24 23:28:22 -0400

    First commit

---
+++ app/src/main/java/com/example/ertzil/reconocimiento/DetectionActivity.java
@@ -0,0 +1,70 @@
+package com.example.ertzil.reconocimiento;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.widget.Toast;
+
+//import com.google.android.gms.samples.vision.face.patch.SafeFaceDetector;
+import com.example.ertzil.reconocimiento.patch.SafeFaceDetector;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+import java.io.InputStream;
+
+public class DetectionActivity extends AppCompatActivity {
+    private static final String TAG = "DetectionActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detection);
+
+        InputStream stream = getResources().openRawResource(R.raw.face);
+        Bitmap bitmap = BitmapFactory.decodeStream(stream);
+
+        FaceDetector detector = new FaceDetector.Builder(getApplicationContext())
+                .setTrackingEnabled(false)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .build();
+        Detector<Face> safeDetector = new SafeFaceDetector(detector);
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+        SparseArray<Face> faces = safeDetector.detect(frame);
+
+        if (!safeDetector.isOperational()) {
+            // Note: The first time that an app using face API is installed on a device, GMS will
+            // download a native library to the device in order to do detection.  Usually this
+            // completes before the app is run for the first time.  But if that download has not yet
+            // completed, then the above call will not detect any faces.
+            //
+            // isOperational() can be used to check if the required native library is currently
+            // available.  The detector will automatically become operational once the library
+            // download completes on device.
+            Log.w(TAG, "Face detector dependencies are not yet available.");
+
+            // Check for low storage.  If there is low storage, the native library will not be
+            // downloaded, so detection will not become operational.
+            IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+            boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
+
+            if (hasLowStorage) {
+                Toast.makeText(this, R.string.low_storage_error, Toast.LENGTH_LONG).show();
+                Log.w(TAG, getString(R.string.low_storage_error));
+            }
+        }
+
+        FaceView overlay = (FaceView) findViewById(R.id.faceView);
+        overlay.setContent(bitmap, faces);
+
+        // Although detector may be used multiple times for different images, it should be released
+        // when it is no longer needed in order to free native resources.
+        safeDetector.release();
+    }
+}
